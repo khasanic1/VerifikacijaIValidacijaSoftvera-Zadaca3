6b5064c5b3e29960f8250eb160272d886dab481a
ahodzic11 <ahodzic11@etf.unsa.ba>
2022-01-08 11:30:42 +0100

    Priprema za drugi zadatak

---
+++ Pijaca/Pijaca/TestTuning.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pijaca;
+using System;
+using System.Collections.Generic;
+
+namespace Pijaca
+{
+    [TestClass]
+    public class TestTuning
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Pijaca/TestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pijaca;
+using System;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class TestTuning
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Tržnica t = new Tržnica();
+            DateTime datumOtvaranja = new DateTime(2021, 1, 2);
+
+
+            int x = 0;
+
+            for (int i=0; i<10000000; i++)
+            {
+                Prodavač prodavač = new Prodavač("Ime", "124", System.DateTime.Now, 10);
+                t.RadSaProdavačima(prodavač, "Dodavanje", 20);
+
+                Prodavač prodavač2 = new Prodavač("Ime", "1245", System.DateTime.Now, 10);
+                t.RadSaProdavačima(prodavač2, "Izmjena", 20);
+                t.RadSaProdavačima(prodavač, "Brisanje", 20);
+            }
+
+            int y = 0;
+        }
+
+    }
+}
